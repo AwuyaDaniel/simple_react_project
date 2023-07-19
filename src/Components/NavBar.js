2c4ac5f922323c5936ca4441633539569a7de031
awuyadaniel <awuyadanielz@gmail.com>
2023-07-19 14:28:52 +0100

    NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -30,7 +30,7 @@ return (
                               d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
-                <div className={`${toggle} w-full md:block md:w-auto`} id="navbar-default">
+                <div className={`${toggle} w-full mix-blend-normal md:block md:w-auto`} id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="#"
