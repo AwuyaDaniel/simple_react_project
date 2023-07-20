40cd46d5d21ca29a3b2a003ce64011a4724c787b
awuyadaniel <awuyadanielz@gmail.com>
2023-07-20 11:32:30 +0100

    Navbar update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -61,25 +61,26 @@ const NavBar = () => {
                         </div>
                         <li>
                             <a href="#"
-                               className=" border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-black rounded md:px-8 md:text-xl md:border-0 hover:bg-gray-100 md:bg-transparent md:p-0 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                               className=" border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-black rounded md:px-6 md:text-xl md:border-0 hover:bg-gray-100 md:bg-transparent md:p-0 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >Home</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 md:text-gray-900 md:px-8 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 md:text-gray-900 md:px-6 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-gray-900 md:px-8 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-gray-900 md:px-6 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-gray-900 md:px-8 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Project</a>
+                               className="border-b rounded-none border-slate-700 block py-4 pl-3 pr-4 text-gray-900 md:px-6 rounded md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Project</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="block py-4 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-xl md:px-8 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pages</a>
+                               className="block py-4 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-xl md:px-6 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pages</a>
                         </li>
+
                         <div className="md:hidden">
                             <h1 className="text-white py-4 pl-3 pr-4 text-2xl font-bold">Get In Touch</h1>
                             <div className="flex flex-row">
@@ -135,6 +136,31 @@ const NavBar = () => {
                             </div>
                             <div>
                                 <div className="border-b rounded-none border-slate-700 py-6"></div>
+                            </div>
+                        </div>
+                        <div className="md:block flex md:order-2 hidden">
+                            <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                                    aria-expanded="false"
+                                    className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                     fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                </svg>
+                                <span className="sr-only">Search</span>
+                            </button>
+                            <div className="relative hidden md:block">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    </svg>
+                                    <span className="sr-only">Search icon</span>
+                                </div>
+                                <input type="text" id="search-navbar"
+                                       className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="Search..."></input>
                             </div>
                         </div>
                     </ul>
